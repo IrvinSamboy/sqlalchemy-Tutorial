3514be59ec0fc54b9c9dd5dda2864c134c3a2b0d
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-03-12 12:42:42 -0400

    Fixing table name

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ class Base(DeclarativeBase):
     pass
 
 class Roles(Base):
-    __tablename__ - "roles"
+    __tablename__ = "roles"
 
     idRole: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     roleName: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    
 
