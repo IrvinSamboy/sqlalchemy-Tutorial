1282174147f6f02f40cbc6884f47d9221d4dccaa
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-03-12 15:26:21 -0400

    importing necesary files in tables.py

--- db/tables.py
+++ db/tables.py
@@ -1,13 +1,14 @@
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+from . import db
 
-class Roles(Base):
+class Roles(db.Base):
     __tablename__ = "roles"
 
     idRole: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     roleName: Mapped[str] = mapped_column(String(100), nullable=False)
 
-class User(Base):
+class User(db.Base):
     __tablename__ = "user"
 
     idUser: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
